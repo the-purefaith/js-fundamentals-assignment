9f3d39ea164df95a34ad68461993a0a73eaac8bd
David Ajibola <ajiboladavid0963@gmail.com>
2025-12-18 17:23:19 +0100

    Add challenge2: Simple Calculator with all arithmetic operations

--- challenge2.js
+++ challenge2.js
@@ -6,4 +6,4 @@ console.log("Subtraction:", num1 - num2);
 console.log("Multiplication:", num1 * num2); 
 console.log("Division:", num1 / num2);      
 console.log("Modulus:", num1 % num2);     
-console.log("Exponentiation:", num1 ** num2);
+console.log("Exponentiation:", num1 ** num2); // 1000
