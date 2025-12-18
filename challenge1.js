91772c6a233d07197e2b85c067991e4b88d46f8b
David Ajibola <ajiboladavid0963@gmail.com>
2025-12-18 16:05:07 +0100

    Complete Challenge 1: Personal Information

---
+++ challenge1.js
@@ -0,0 +1,12 @@
+const fullName = "Ajibola Faith";
+const age = 21;
+const favoriteProgrammingLanguage = "JavaScript";
+const isStudent = true;
+const currentYear = 2025;
+
+const introduction = `Hello! My name is ${fullName}.
+I am ${age} years old and currently in the year ${currentYear}.
+My favorite programming language is ${favoriteProgrammingLanguage}.
+Am I a student? ${isStudent}`;
+
+console.log(introduction);
