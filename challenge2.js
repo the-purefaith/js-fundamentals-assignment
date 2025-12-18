208fb8c005987357f2622c2b652e415ecce24bf3
David Ajibola <ajiboladavid0963@gmail.com>
2025-12-18 17:17:17 +0100

    Add challenge3: Even/Odd checker with loop and counter

---
+++ challenge2.js
@@ -0,0 +1,9 @@
+const num1 = 10;
+const num2 = 3;
+
+console.log("Addition:", num1 + num2);    
+console.log("Subtraction:", num1 - num2);  
+console.log("Multiplication:", num1 * num2); 
+console.log("Division:", num1 / num2);      
+console.log("Modulus:", num1 % num2);     
+console.log("Exponentiation:", num1 ** num2);
